443ec2f2d3267e0bc585cbd62ceba0331ff32733
keemo <keemowatson1@gmail.com>
2019-11-18 11:18:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -20,7 +20,7 @@ def directors_totals(nds)
     #   result[director[:name]] = gross
     # end
     #   result
-
+pp directors_database
 
     directorCount = 0
     grossCount = 0
@@ -37,9 +37,9 @@ def directors_totals(nds)
          while grossCount == directors_database[directorCount][:movies].count
            grossCount = 0
            directorCount += 1
-          #  if directorCount == directors_database.count
-          #    break
-        #  end
+           if directorCount == directors_database.count
+             break
+         end
        end
      end
      result
